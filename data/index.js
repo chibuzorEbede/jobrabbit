37a138ee7eb34f8d586b7552f6548a96605581d8
chibuzorEbede <chibuzorebede@gmail.com>
2024-04-18 08:50:06 -0500

    landing page initial commit

---
+++ data/index.js
@@ -0,0 +1,62 @@
+const jobs = [
+  {
+    id: 1,
+    title: "Art Director at Shopify",
+    location: "Mississauga, Ontario",
+    type: "Part-Time",
+    description:
+      "As an Art Director at jobrabbit, you will lead our creative team in conceptualizing and executing visually stunning and strategically sound designs across various platforms. You will collaborate closely with our clients, account managers, and other stakeholders to understand project objectives and deliver impactful creative solutions.",
+    imgUrl: "listing-shopify-featured-image-1.jpg",
+    link: "s",
+  },
+  {
+    id: 2,
+    title: "Salesman",
+    location: "Nicaragua, Bolivia",
+    type: "Full time",
+    description:
+      "As an Art Salesman at Jobrabbit, you will play a pivotal role in facilitating the sale of artwork while providing exceptional customer service to our clients. You will engage with customers in a professional and knowledgeable manner, guiding them through our diverse collection of artwork and assisting them in selecting pieces that align with their tastes and preferences.",
+    imgUrl: "listing-airbnb-featured-image-1.jpg",
+    link: "e",
+  },
+  {
+    id: 3,
+    title: "Code Chef",
+    location: "San Antonio, USA",
+    type: "Flexible",
+    description:
+      "We are seeking a talented and passionate chef to join our team. As a chef at jobrabbit, you will be responsible for leading our kitchen team in the preparation and execution of our menu items. You will work closely with the kitchen staff to ensure that all dishes are prepared to the highest standards of quality, taste, and presentation.",
+    imgUrl: "listing-squarespace-featured-image-1.jpg",
+    link: "e",
+  },
+];
+const candidates = [
+  {
+    id: 1,
+    url: "s",
+    imgUrl: "candidate.png",
+    name: "Amanda Cook",
+    location: "California",
+    description:
+      "Spent several years working on sheep on Wall Street. Had moderate success investing in Yugos on Wall Street.",
+  },
+  {
+    id: 2,
+    url: "c",
+    imgUrl: "candidate.png",
+    name: "Amanda Cook",
+    location: "California",
+    description:
+      "Spent several years working on sheep on Wall Street. Had moderate success investing in Yugos on Wall Street.",
+  },
+  {
+    id: 3,
+    url: "d",
+    imgUrl: "candidate.png",
+    name: "Amanda Cook",
+    location: "California",
+    description:
+      "Spent several years working on sheep on Wall Street. Had moderate success investing in Yugos on Wall Street.",
+  },
+];
+module.exports = { jobs, candidates };
